10afe40eda5008441a6e91da0430a6ba94a7ecce
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-09-20 02:02:35 +0530

    make all string values and print it

--- src/books/Books.java
+++ src/books/Books.java
@@ -11,5 +11,17 @@ public class Books {
         } else {
             sc.nextLine();
         }
+        while (n > 0) {
+            String bookName = sc.nextLine();
+            String authorName = sc.nextLine();
+            String ISBN = sc.nextLine();
+            System.out.println("-----------------------------");
+            System.out.println("Book Name:\t" + bookName);
+            System.out.println("Author Name:\t" + authorName);
+            System.out.println("ISBN:\t" + ISBN);
+            System.out.println("-----------------------------");
+            n = n - 1;
+
+        }
     }
 }
