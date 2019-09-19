7e4ae0f0d9f7f73eeacf87ddb07571cfd5184391
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-09-20 02:00:27 +0530

    added if else cndition acc. to question

--- src/books/Books.java
+++ src/books/Books.java
@@ -6,4 +6,10 @@ public class Books {
     public void display() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            sc.nextLine();
+        }
     }
+}
